2f63ab65c4b0a2523064cb208857b59ee7acbc0c
wtczz <2488130937@qq.com>
2025-01-10 19:12:14 +0800

    Vue001

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<Home></Home>
+</template>
+
+<script>
+	import Home from "./components/AppHome.vue"
+	export default {
+		name:"App",
+		components:{
+			Home:Home,
+		},
+	};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppBody.vue
@@ -0,0 +1,62 @@
+<template>
+	<el-container style="height: 100%;">
+		<el-aside width="200px" style="background-color: #f1f1f1;">
+			<div></div>
+			<el-menu mode="vertical" background-color="#f1f1f1" text-color="#777777" 
+			active-text-color="#000000" :default-active="0" @select="selectItem">
+				<el-menu-item v-for="item in items" :index="item.index" :key="item.index">
+					<div id="text">{{item.title}}</div>
+				</el-menu-item>
+			</el-menu>
+		</el-aside>
+		<el-main>
+			<MarkDown :content="content"></MarkDown>
+	    </el-main>
+	</el-container>
+</template>
+
+<script>
+import MarkDown from "./MarkDown.vue"
+import FileManager from "../tools/FileManager";
+export default	{
+	mounted () {
+		FileManager.getPostContent(this.topic,this.items[this.currentIndex].title).then((res) => {
+			this.content = res.data;
+		})
+	},
+	props: ["items","topic"],
+	data() {
+		return {
+			currentIndex:0,
+			content:""
+		}
+	},
+	components:	{
+	MarkDown:MarkDown,
+	},
+	methods:{
+		selectItem(index) {
+			this.currentIndex = index
+		}
+	},
+	watch:{
+		currentIndex:function(val) {
+			FileManager.getPostContent(this.topic,this.items[val].title).then((res) =>{
+				this.content = res.data;
+			})
+		},
+		topic:function(val) {
+			FileManager.getPostContent(val,this.items[this.currentIndex].title).
+			then((res) =>{
+				this.content = res.data;
+			})
+		}
+	}
+};
+</script>
+
+<style scoped>
+.el-menu-item.is-action{
+	background-color: #ffffff !important;
+}
+</style>
